451e23c6624754d691f13c1ee58deecb1ea866b6
Spector <33796688+Spector0@users.noreply.github.com>
2018-04-12 17:29:24 +0530

    Use device tree from bacon-stuff

--- script.sh
+++ script.sh
@@ -11,7 +11,7 @@ rm -rf hardware/qcom/media/msm8974
 git clone https://github.com/Fabulous-Oreo/hardware_qcom_media-caf -b oreo-mr1 hardware/qcom/media/msm8974
 git clone https://github.com/Fabulous-Oreo/hardware_qcom_wlan -b oreo-mr1 hardware/qcom/wlan
 git clone https://github.com/Fabulous-Oreo/hardware_qcom_camera -b oreo-mr1 hardware/qcom/camera
-git clone https://github.com/Spector0/device_oneplus_bacon -b dot-o device/oneplus/bacon
+git clone https://github.com/Bacon-stuff/device_oneplus_bacon -b aex-oss device/oneplus/bacon
 git clone https://github.com/Fabulous-Oreo/kernel_oneplus_msm8974 -b oreo-mr1 kernel/oneplus/msm8974
 git clone https://github.com/Fabulous-Oreo/vendor_oneplus -b oreo-mr1 vendor/oneplus
 git clone https://github.com/Fabulous-Oreo/device_oppo_common -b oreo-mr1 device/oppo/common
