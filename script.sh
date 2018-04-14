680a48e2f3ced53e8d9ed2d06dd19c9af60541af
Spector <33796688+Spector0@users.noreply.github.com>
2018-04-14 09:48:28 +0530

    Clone audio hals from fabulous-oreo repo

--- script.sh
+++ script.sh
@@ -5,6 +5,9 @@ rm -rf hardware/qcom/media-caf
 rm -rf hardware/qcom/wlan
 rm -rf hardware/qcom/wlan-caf
 rm -rf hardware/qcom/camera
+rm -rf hardware/qcom/audio
+rm -rf hardware/qcom/audio-caf
+git clone https://github.com/Fabulous-Oreo/hardware_qcom_audio-caf -b oreo-mr1 hardware/qcom/audio-caf
 git clone https://github.com/Fabulous-Oreo/hardware_qcom_display -b oreo-mr1 hardware/qcom/display
 git clone https://github.com/Fabulous-Oreo/hardware_qcom_media -b oreo-mr1 hardware/qcom/media
 rm -rf hardware/qcom/media/msm8974
