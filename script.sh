a3ba33058e4e37b1d7018be7b26210bd580f01bd
Spector <33796688+Spector0@users.noreply.github.com>
2018-04-16 21:32:21 +0530

    Clone device related trees from candy repo

--- script.sh
+++ script.sh
@@ -14,10 +14,10 @@ rm -rf hardware/qcom/media/msm8974
 git clone https://github.com/Fabulous-Oreo/hardware_qcom_media-caf -b oreo-mr1 hardware/qcom/media/msm8974
 git clone https://github.com/Fabulous-Oreo/hardware_qcom_wlan -b oreo-mr1 hardware/qcom/wlan
 git clone https://github.com/Fabulous-Oreo/hardware_qcom_camera -b oreo-mr1 hardware/qcom/camera
-git clone https://github.com/Spector0/device_oneplus_bacon -b nitrogen device/oneplus/bacon
-git clone https://github.com/Fabulous-Oreo/kernel_oneplus_msm8974 -b oreo-mr1 kernel/oneplus/msm8974
-git clone https://github.com/Fabulous-Oreo/vendor_oneplus -b oreo-mr1 vendor/oneplus
-git clone https://github.com/Fabulous-Oreo/device_oppo_common -b oreo-mr1 device/oppo/common
+git clone https://github.com/Spector0/device_oneplus_bacon-1 -b nos device/oneplus/bacon
+git clone https://github.com/CandyDevices/kernel_oppo_msm8974 -b c8.1 kernel/oppo/msm8974
+git clone https://github.com/CandyDevices/vendor_oneplus -b c8.1 vendor/oneplus
+git clone https://github.com/CandyDevices/device_oppo_msm8974-common -b c8.1 device/oppo/msm8974-common
 git clone https://github.com/Fabulous-Oreo/external_sony_boringssl-compat -b oreo-mr1 external/sony/boringssl-compat
 git clone https://github.com/Fabulous-Oreo/packages_apps_Snap-bacon -b oreo-mr1 packages/apps/Snap-bacon
 rm -rf device/oneplus/bacon/dataservices	
