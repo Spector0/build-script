a01fe948653ddb7529befb75e94d5acb1f92da65
Spector <33796688+Spector0@users.noreply.github.com>
2018-12-29 13:20:12 +0530

    fix derp

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/Spector0/device_zuk_z2_plus -b pie device/zuk/z2_plus
 git clone https://github.com/Spector0/kernel_zuk_msm8996 -b pie kernel/zuk/msm8996
 git clone https://github.com/Spector0/vendor_zuk -b pie vendor/zuk
-git clone https://github.com/Spector0/device_zuk_msm8996-common -b device/zuk/msm8996-common
+git clone https://github.com/Spector0/device_zuk_msm8996-common -b pie device/zuk/msm8996-common
 rm -rf device/zuk/msm8996-common/pocketmode
